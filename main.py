de975d4b429938ef3b99fc8a91bdd2fe71448dd3
hyy230 <2638936581@qq.com>
2025-09-26 12:14:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    else:
-        return False
-
 try:
-    year = int(input("请输入年份: "))
-    
-    if is_leap_year(year):
-        print(f"{year}年是闰年")
+    year = int(input("请输入年份："))
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        print(f"{year} 是闰年")
     else:
-        print(f"{year}年不是闰年")
-        
+        print(f"{year} 不是闰年")
 except ValueError:
-    print("错误：请输入有效的年份数字！")
+    print("输入无效，请输入数字年份")
